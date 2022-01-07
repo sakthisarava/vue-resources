5b4fbcb49d8461a25ff57e9e1a04fadd991d98d6
sakthi s <velsakthis5301@gmail.com>
2022-01-07 14:44:51 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<the-header title="RememberMe"></the-header>
+<the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+export default {
+    components: {
+        TheHeader,
+        TheResources
+    },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&family=Lora:ital,wght@1,400;1,500&family=Poppins&family=Roboto:wght@500&family=Varela&family=Varela+Round&display=swap');
+
+*{
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
